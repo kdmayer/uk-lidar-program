ff152ffaa812b4cfd536d7e0a2b41716a31b6b8c
Kevin <kdmayer92@gmail.com>
2022-08-30 17:09:41 -0700

    initial restructuring completed

---
+++ src/tile_scraper.py
@@ -0,0 +1,152 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+
+from typing import List
+from utils import compress_as_zip, download_url
+
+import os
+
+class TileScraper:
+
+    def __init__(self, headless: bool=False, output_dir: str=None, aoi_shp_path: str=None):
+        """
+        Instantiate Firefox browser instance.
+        """
+        opts = Options()
+        opts.headless = headless
+        self.browser = Firefox(options=opts)
+        self.output_dir = output_dir
+        self.aoi_shp_path = aoi_shp_path
+        self.aoi_zip_path = compress_as_zip(self.aoi_shp_path)
+
+    def open_data_gov_uk(self):
+
+        print("...waiting for data.gov.uk page to load.")
+
+        self.browser.get("https://environment.data.gov.uk/DefraDataDownload/?Mode=survey")
+
+        self.wait = WebDriverWait(self.browser, 300)
+
+        print("...page has loaded.")
+
+    def upload_aoi_to_data_gov_uk(self):
+
+        print("...waiting for shapefile to load")
+
+        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#fileid")))
+
+        self.browser.find_element(by=By.CSS_SELECTOR, value="#fileid").send_keys([self.aoi_zip_path])
+
+        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".grid-item-container")))
+
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".grid-item-container")))
+
+        self.E1 = self.browser.find_element(by=By.CSS_SELECTOR, value=".grid-item-container")
+
+        print("...shapefile has loaded")
+
+    def list_available_data_gov_uk_products(self):
+
+        print("...waiting for available products to load")
+
+        while True:  # hack :(
+
+            try:
+                self.E1.click()
+
+            except ElementNotInteractableException as e:
+                break
+
+            self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#productSelect")))
+
+            self.list_of_products = [
+                x.get_attribute("value")
+                for x in Select(
+                    self.browser.find_element(by=By.CSS_SELECTOR, value="#productSelect")
+                ).options
+            ]
+
+        print(f"List of products: {self.list_of_products}")
+
+    def download_desired_data_gov_uk_product(self, desired_product_list: List=None, desired_year: str=None, latest: bool=True):
+
+        for desired_product in desired_product_list:
+
+            if desired_product not in self.list_of_products:
+                print(f"Desired product: {desired_product} is not available.")
+                continue
+
+            else:
+                xPath = f'//*[@id="productSelect"]/option[{self.list_of_products.index(desired_product) + 1}]'
+
+                self.wait.until(EC.presence_of_element_located((By.XPATH, xPath)))
+                self.browser.find_element(by=By.XPATH, value=xPath).click()
+
+                available_years_list = [
+                    x.get_attribute("value")
+                    for x in Select(
+                        self.browser.find_element(by=By.CSS_SELECTOR, value="#yearSelect")
+                    ).options
+                ]
+
+                if desired_year in available_years_list:
+                    year_element = [f'//*[@id="yearSelect"]/option[{available_years_list.index(desired_year) + 1}]']
+
+                elif (desired_year not in available_years_list) and latest:
+
+                    year_element = ['//*[@id="yearSelect"]/option[1]']
+
+                else:
+                    print(f"Desired product is not available.")
+                    continue
+
+                year_to_be_downloaded = available_years_list[int(year_element[0].split("[")[-1][:-1]) - 1]
+
+                print(f"Year to be downloaded: {year_to_be_downloaded}")
+                print(f"Desired year: {desired_year}")
+                print(f"Available years: {available_years_list}")
+
+                self.wait.until(EC.presence_of_element_located((By.XPATH, year_element[0])))
+                self.browser.find_element(by=By.XPATH, value=year_element[0]).click()
+                link_idx = 1
+
+                while True:
+                    try:
+                        file_url = self.browser.find_element(
+                            by=By.CSS_SELECTOR,
+                            value=f".data-ready-container > a:nth-child({link_idx})",
+                        ).get_attribute("href")
+
+                        file_path = os.path.join(
+                            self.output_dir,
+                            self.aoi_zip_path.split("/")[-1][:-4] + "-" +
+                            file_url.split("/")[-1],
+                        )
+
+                        if not os.path.isfile(file_path):
+                            download_url(file_url, file_path)
+                            print(f"Saved to: {file_path}")
+
+                        link_idx += 1
+
+                    except NoSuchElementException:
+                        print(f"{link_idx - 1} files downloaded for {year_to_be_downloaded}")
+                        break
+                    except Exception as err:
+                        print(err)
+
+        self.browser.close()
+
+
+
+
+
+
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -1,185 +1,29 @@
-import os
-import glob
-import geopandas as gp
-
-from zipfile import ZipFile
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-from selenium.common.exceptions import (
-    NoSuchElementException,
-    ElementNotInteractableException,
-    TimeoutException,
-)
-
-import urllib.request
+from os.path import dirname
 from tqdm.auto import tqdm
 
+import urllib.request
+import zipfile
+import os
 
-def download_tile(
-    zipf,
-    download=False,
-    product_list=None,
-    verbose=True,
-    download_dir=False,
-    headless=True,
-    browser="chrome",
-    year="latest",
-    all_years=False,
-    print_only=True,
-):
 
-    if browser == "firefox":
-        if verbose:
-            print("using FIREFOX")
-        from selenium.webdriver.firefox.options import Options
+def compress_as_zip(aoi_file_path: str = None):
+    aoi_dir = dirname(os.path.abspath(aoi_file_path))
 
-        options = Options()
-        options.headless = headless
-        driver = webdriver.Firefox(options=options)
-    else:
-        if verbose:
-            print("using CHROME")
-        from selenium.webdriver.chrome.options import Options
-        options = Options()
-        options.headless = headless
-        # driver = webdriver.Chrome(chromedriver_binary.chromedriver_filename, options=options)
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    aoi_name = aoi_file_path.split("/")[-1]
+    aoi_name = aoi_name[:-4]
 
-    if verbose:
-        print("...waiting for page to load")
-    driver.get("https://environment.data.gov.uk/DefraDataDownload/?Mode=survey")
-    wait = WebDriverWait(driver, 300)
+    zip_file_path = os.path.join(aoi_dir, aoi_name + ".zip")
 
-    if verbose:
-        print("...waiting for shapefile to load")
-    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#fileid")))
-    driver.find_element(by=By.CSS_SELECTOR, value="#fileid").send_keys([zipf])
+    list_of_aoi_files = []
 
-    wait.until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, ".grid-item-container"))
-    )
-    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".grid-item-container")))
-    #    try:
-    #        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".grid-item-container")))
-    #    except TimeoutException:
-    #        if driver.find_element_by_css_selector( 'div.errorsContainer:nth-child(1)').is_displayed():
-    #            raise Exception("The uk_raster Polygon uploaded exceeds the maximum number of vertices allowed. Use a less complex polygon The maximum vertex count is : 1000")
-    E1 = driver.find_element(by=By.CSS_SELECTOR, value=".grid-item-container")
+    for file in os.listdir(aoi_dir):
+        if file.split(".")[0] == aoi_name:
+            list_of_aoi_files.append(os.path.join(aoi_dir, file))
 
-    if verbose:
-        print("...waiting for available products to load")
-    while True:  # hack :(
-        try:
-            E1.click()
-        except ElementNotInteractableException as e:
-            break
+    with zipfile.ZipFile(zip_file_path, 'w') as zipMe:
+        [zipMe.write(file, compress_type=zipfile.ZIP_DEFLATED) for file in list_of_aoi_files]
 
-    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#productSelect")))
-    products = [
-        x.get_attribute("value")
-        for x in Select(
-            driver.find_element(by=By.CSS_SELECTOR, value="#productSelect")
-        ).options
-    ]
-    print(products)
-
-    for product in product_list:
-        if product not in products:
-            print("product not available")
-        else:
-            xP = '//*[@id="productSelect"]/option[{}]'.format(
-                products.index(product) + 1
-            )
-            wait.until(EC.presence_of_element_located((By.XPATH, xP)))
-            driver.find_element(by=By.XPATH, value=xP).click()
-
-            years = [
-                x.get_attribute("value")
-                for x in Select(
-                    driver.find_element(by=By.CSS_SELECTOR, value="#yearSelect")
-                ).options
-            ]
-            if year == "latest":
-                xY = ['//*[@id="yearSelect"]/option[1]']
-                if verbose:
-                    print(
-                        "downloading data for: {}".format(
-                            years[int(xY[0].split("[")[-1][:-1]) - 1]
-                        )
-                    )
-            elif not all_years:
-                if year not in years:
-                    print(
-                        "no {} data available for {}, available years are {}".format(
-                            product, year, ", ".join(years)
-                        )
-                    )
-                    continue
-                #                     raise YearError('Years available are {}'.format(years))
-                xY = [
-                    '//*[@id="yearSelect"]/option[{}]'.format(
-                        years.index(str(year)) + 1
-                    )
-                ]
-            else:
-                most_recent = int(years[0])
-                if most_recent < int(year):
-                    #                     raise YearError('Years available are {}'.format(years))
-                    print(
-                        "no {} data available for {}, available years are {}".format(
-                            product, year, ", ".join(years)
-                        )
-                    )
-                    continue
-                available_years = [
-                    str(y) for y in range(int(year), most_recent + 1) if str(y) in years
-                ]
-                xY = [
-                    '//*[@id="yearSelect"]/option[{}]'.format(years.index(y) + 1)
-                    for y in available_years
-                ]
-
-            for xYs in xY:
-                current = years[int(xYs.split("[")[-1][:-1]) - 1]
-                wait.until(EC.presence_of_element_located((By.XPATH, xYs)))
-                driver.find_element(by=By.XPATH, value=xYs).click()
-                linki = 1
-                while True:
-                    try:
-                        href = driver.find_element(
-                            by=By.CSS_SELECTOR,
-                            value=".data-ready-container > a:nth-child({})".format(
-                                linki
-                            ),
-                        ).get_attribute("href")
-                        file_loc = os.path.join(
-                            os.path.split(zipf[0])[0]
-                            if not download_dir
-                            else download_dir,
-                            href.split("/")[-1],
-                        )
-                        if print_only:
-                            print("available:", href)
-                        else:
-                            if not os.path.isfile(file_loc):
-                                download_url(href, file_loc)
-                                if verbose:
-                                    print("saved to:", file_loc)
-                        linki += 1
-                    except NoSuchElementException:
-                        if verbose and not print_only:
-                            print(linki - 1, "files downloaded for {}".format(current))
-                        break
-                    except Exception as err:
-                        print(err)
-
-    return driver
+    return zip_file_path
 
 
 class DownloadProgressBar(tqdm):
@@ -189,62 +33,8 @@ class DownloadProgressBar(tqdm):
         self.update(b * bsize - self.n)
 
 
-def download_url(url, output_path):
-
+def download_url(file_url, file_path):
     with DownloadProgressBar(
-        unit="B", unit_scale=True, miniters=1, desc=url.split("/")[-1]
+            unit="B", unit_scale=True, miniters=1, desc=file_url.split("/")[-1]
     ) as t:
-        urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
-
-
-def num_vertices(shp):
-
-    N = 0
-    for i, row in shp.iterrows():
-        if row.geometry.type.startswith(
-            "Multi"
-        ):  # It's better to check if multigeometry
-            for part in row.geometry:  # iterate over all parts of multigeometry
-                N += len(part.exterior.coords)
-        else:  # if single geometry like point, linestring or polygon
-            N += len(row.geometry.exterior.coords)
-    return N
-
-
-class YearError(Exception):
-    pass
-
-
-def tile_input(shp, args):
-
-    osgb = gp.read_file("/Users/kevin/Projects/uk-lidar-program/assets/aoi/uk_subset_5km_raster.shp")
-
-    osgb_sindex = osgb.sindex
-    tile_index = [
-        list(osgb_sindex.intersection(row.geometry.bounds)) for row in shp.itertuples()
-    ][0]
-    for idx in tile_index:
-        tmp_shp = gp.GeoDataFrame(geometry=[osgb.loc[idx].geometry], crs="EPSG:27700")
-        if tmp_shp.intersects(shp).values[0]:
-            tile_tmp = os.path.join(args.tmp_d, "{}_{}".format(args.tmp_n, idx))
-            gp.GeoDataFrame(geometry=[osgb.loc[idx].geometry]).to_file(
-                tile_tmp + ".shp"
-            )
-            with ZipFile(os.path.join(args.tmp_d, tile_tmp + ".zip"), "w") as zipObj:
-                [zipObj.write(f) for f in glob.glob(tile_tmp + "*")]
-            if args.verbose:
-                print("zip file saved to:", os.path.join(args.tmp_d, tile_tmp + ".zip"))
-            driver = download_tile(
-                tile_tmp + ".zip",
-                print_only=args.print_only,
-                product_list=args.required_products,
-                headless=True,
-                year=args.year,
-                all_years=args.all_years,
-                download_dir=args.odir,
-                browser=args.browser,
-                verbose=args.verbose,
-            )
-            if not args.open_browser:
-                driver.close()
-            # break
+        urllib.request.urlretrieve(file_url, filename=file_path, reporthook=t.update_to)
